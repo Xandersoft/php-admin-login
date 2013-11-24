ac606da00644d09bec9df5c3d07f51c6e9543a47
Sean Webber <yazziecast@gmail.com>
2013-11-23 19:22:39 -0500

    Example Password Protected Page

---
+++ admin.php
@@ -0,0 +1,10 @@
+<?php include 'auth.php'; // "auth.php" include() needed to password protect this page. ?>
+
+<html>
+<title>This page is password protected | php-admin-login</title>
+</head>
+<body>
+<p>This page is password protected.<br><br>
+<a href="login.php?action=logout">Log Out</a></p>
+</body>
+</html>
